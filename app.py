8192a44f18a8a7d1c42d17040c17595f822c133f
Joel Alftberg <pi@servicedesk01.lan>
2021-09-03 23:40:01 +0200

    Added current temperature

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import psycopg2
-import db.sensordb
+import db.sensordb 
+import re
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
-    return "<h1>Main page<h1><br><h3>{{ }}</h3>"
+    current_temperature = db.sensordb.sql_current('celsius')
+    return render_template('index.html', temperature=current_temperature[0])
--- db/sensordb.py
+++ db/sensordb.py
@@ -1,13 +1,19 @@
+import psycopg2
+
+connection = psycopg2.connect(database="sensordb", user="pi", password="teampurple", host="127.0.0.1", port="5432")
+
+cursor = connection.cursor()
+
 def sql_average(column, interval):
     
     cursor.execute(f"SELECT avg({column}) FROM temperature WHERE date BETWEEN (SELECT NOW() - interval '{interval}') AND NOW()")
     result = cursor.fetchone()
-
+    print(result)
     return result
 
 def sql_current(column):
 
-    cursor.execute(f"SELECT {column} FROM temperature WHERE date = NOW()")
+    cursor.execute(f"SELECT {column} FROM temperature WHERE date BETWEEN (SELECT NOW() - interval '10 seconds') AND NOW()")
     result = cursor.fetchone()
 
     return result
@@ -15,5 +21,4 @@ def sql_current(column):
 
 ### EXAMPLE FUNCTION CALL ###
 
-#print(sql_average('celsius', '7 days'))
-
+#sql_average('celsius', '7 days')
