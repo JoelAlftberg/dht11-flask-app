8402fc833784db679b8b0c4f964066edc9989584
Joel Alftberg <joel.alftberg98@gmail.com>
2021-09-09 21:13:21 +0200

    Rewrote psql_refresh.sql

--- psql_refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-psql -d sensordb -c "REFRESH MATERIALIZED VIEW celsius_daily_average;"
-psql -d sensordb -c "REFRESH MATERIALIZED VIEW celsius_weekly_average;"
-psql -d sensordb -c "REFRESH MATERIALIZED VIEW celsius_monthly_average;"
-psql -d sensordb -c "REFRESH MATERIALIZED VIEW humidity_daily_average;"
-psql -d sensordb -c "REFRESH MATERIALIZED VIEW humidity_monthly_average;"
---
+++ psql_refresh.sql
@@ -0,0 +1,5 @@
+REFRESH MATERIALIZED VIEW celsius_daily_average;
+REFRESH MATERIALIZED VIEW celsius_weekly_average;
+REFRESH MATERIALIZED VIEW celsius_monthly_average;
+REFRESH MATERIALIZED VIEW humidity_daily_average;
+REFRESH MATERIALIZED VIEW humidity_monthly_average;
